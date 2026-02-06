83bfd03e564cadcfbc3f34afc229cdb0371cbf63
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-02-06 22:10:07 +0530

    ex 16b works with negative number test formatCurrency

--- exercises/lesson 16 testing jasmine/tests/utils/moneyTest.js
+++ exercises/lesson 16 testing jasmine/tests/utils/moneyTest.js
@@ -16,4 +16,8 @@ describe("Test suite: formatCurrency", () => {
   it("rounds down to the nearest cent", () => {
     expect(formatCurrency(2000.4)).toEqual("20.00");
   });
+
+  it("works with negative numbers", () => {
+    expect(formatCurrency(-400)).toEqual("-4.00");
+  });
 });
