a0202d3a6b1f930971e37e6d36c57ae279ed237a
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-02-08 01:40:09 +0530

    chl ex 16l modify updateDeliveryOption() for productId not in cart to do nothing and create edge case test for it

--- exercises/lesson 16 testing jasmine/data/cart.js
+++ exercises/lesson 16 testing jasmine/data/cart.js
@@ -66,6 +66,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     }
   });
 
+  if (!matchingItem) {
+    return;
+  }
+
   matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
