463230ea289ecd619da5ce230b72cede949ab506
IndrajeetBohra <bohraindrajeet@gmail.com>
2026-02-08 01:58:18 +0530

    done all chl ex 16m edge case in updateDeliveryOption when delivery option does not exist

--- exercises/lesson 16 testing jasmine/data/cart.js
+++ exercises/lesson 16 testing jasmine/data/cart.js
@@ -1,3 +1,5 @@
+import { validDeliveryOption } from "./deliveryOptions.js";
+
 export let cart;
 
 loadFromStorage();
@@ -67,6 +69,10 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   });
 
   if (!matchingItem) {
+    return;
+  }
+
+  if (!validDeliveryOption(deliveryOptionId)) {
     return;
   }
 
--- exercises/lesson 16 testing jasmine/tests/data/cartTest.js
+++ exercises/lesson 16 testing jasmine/tests/data/cartTest.js
@@ -110,7 +110,7 @@ describe("Test suite: removeFromCart()", () => {
   });
 });
 
-describe("Test suite: updateDeliveryOption", () => {
+describe("Test suite: updateDeliveryOption()", () => {
   beforeEach(() => {
     spyOn(localStorage, "setItem");
   });
@@ -157,6 +157,28 @@ describe("Test suite: updateDeliveryOption", () => {
     });
     loadFromStorage();
     updateDeliveryOption("does-not-exist", "3");
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
+    expect(cart[0].quantity).toEqual(1);
+    expect(cart[0].deliveryOptionId).toEqual("1");
+    expect(localStorage.setItem).toHaveBeenCalledTimes(0);
+  });
+
+  it("does nothing if delivery option does not exist", () => {
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 1,
+          deliveryOptionId: "1",
+        },
+      ]);
+    });
+    loadFromStorage();
+    updateDeliveryOption(
+      "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      "does-not-exist",
+    );
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual("e43638ce-6aa0-4b85-b27f-e1d07eb678c6");
     expect(cart[0].quantity).toEqual(1);
